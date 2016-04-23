70381112382a258303d7e0ba2b5607b7b9efacf4
Izak Smit <oegma2@gmail.com>
2016-04-24 01:02:06 +0200

    some more functions

--- echo.erl
+++ echo.erl
@@ -15,4 +15,25 @@ add() ->
          From ! Value*Value,
          add()
     end.
-    
+
+bank(add, {Accno, Val}) ->
+   io:format(">>> Adding to account ~w value ~w~n", [Accno, Val]);
+bank(remove, {Accno, Val}) ->
+   io:format("<<< Removing to account ~w value ~w~n", [Accno, Val]).
+   
+age(X) when X > 16, X < 100 ->
+   io:format("You can drive any car");
+age(_) ->
+   io:format("Not the right time for driving").
+   
+gaurd(X, Y) when X > Y, X > 5 ->
+   X*Y+Y;
+gaurd(_, _) ->
+   bad_value_range.
+   
+even(X) when X rem 2 == 0 -> true;
+even(X) when X rem 2 == 1 -> false.
+
+whatnum(X) when is_integer(X) -> {val, "INT"};
+whatnum(X) when is_float(X) -> {val, "FLOAT"};
+whatnum(_X) -> {val, "NOCLUE"}.
